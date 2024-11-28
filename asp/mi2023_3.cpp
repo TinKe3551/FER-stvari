4ed64f79651a5f065a0b2510169b1350267048da
TinKe3551 <tin.keglevic@gmail.com>
2024-11-28 15:42:46 +0100

    .

--- asp/mi2023_3.cpp
+++ asp/mi2023_3.cpp
@@ -6,9 +6,21 @@ using std::cin, std::cout, std::string;
 
 template<typename T>
 class ListElement {
+
     public:
+
     T item;
     ListElement<T>* next;
+
+    ListElement(T val) {
+        this->item = val;
+        this->next = nullptr;
+    }
+
+    ~ListElement() {
+        cout << "oof\n";
+    }
+
 };
 
 
@@ -16,30 +28,44 @@ template<typename T>
 class MyList {
 
     private:
-    ListElement<T> *head = nullptr;
+    ListElement<T> *head;
 
     public:
+
+    MyList() { this->head = nullptr;}
 
     void AddSorted(T val) {
 
         ListElement<T> *el = this->head;
 
-        ListElement<T> *add = new ListElement<T>;
-        add->item = val;
+        ListElement<T> *add = new ListElement<T>(val);
         add->next = nullptr;
 
-        if (el == nullptr) el = add;
+        if (el == nullptr) {
+            this->head = add;
+            return;
+        }
 
-        while (el->item < val && el->next != nullptr) el = el->next;
+        if (this->head->item > val) {
+            add->next = this->head;
+            this->head = add;
+            return;
+        }
+
+        while (el->next != nullptr && el->next->item < val) el = el->next;
 
         if (el->next == nullptr) {
             el->next = add;
+            return;
         }
+
+       
 
         else {
             ListElement<T> *el2 = el->next;
-            el->next = add;
             add->next = el2;
+            el->next = add;
+            return;
         }
 
     }
@@ -58,7 +84,7 @@ class MyList {
         while (e1 != nullptr) {
             cout << e1->item << " ";
             e1 = e2;
-            e2 = e1->next;
+            if (e1 != nullptr) e2 = e1->next;
         }
 
         cout << "\n";
@@ -69,6 +95,8 @@ class MyList {
     ~MyList() {
         ListElement<T> *e1 = this->head;
 
+        int counter = 0;
+
         if (e1 == nullptr) {
             return;
         }
@@ -78,8 +106,11 @@ class MyList {
         while (e1 != nullptr) {
             delete(e1);
             e1 = e2;
-            e2 = e1->next;
+            if (e1 != nullptr) e2 = e1->next;
+            counter++;
         }
+
+        cout << "deleted list with " << counter << " items\n";
     }
 
 };
@@ -95,6 +126,8 @@ int main(void) {
         cin >> n;
         lista.AddSorted(n);
     }
+
+    lista.Print();
 
     return 0;
 
