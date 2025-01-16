1ae8c038c3fe02690373fe382ce8d699cc5eb95c
TinKe3551 <tk55945@fer.hr>
2025-01-16 14:14:32 +0100

    .

--- dismat/dismat_lab3.cpp
+++ dismat/dismat_lab3.cpp
@@ -4,13 +4,49 @@
 
 using namespace std;
 
+
 map<int, vector<int>> susjedstva;
+vector<int> boje;
+
+
+bool slj(int n, int k) {
+
+    for (int i = 0; i < n; i++) {
+
+        boje[i]++;
+        boje[i] %= k;
+
+        if (boje[i] > 0) break;
+
+    }
+
+    // u početnom stanju popisa boja, u njemu se nalaze samo nule
+    // ako se popis opet nađe u takvom stanju, to znači
+    // da su sve moguće kombinacije boja isprobane
+
+    
+    for (int i = 0; i < n; i++) {
+        // barem jedan broj različit od nule
+        if (boje[i] != 0) return true;
+    }
+
+    // samo nule
+    return false;
+
+}
 
 
 int main(void) {
     
     string  dat;
+
+    #ifndef debug
     cin >> dat;
+    #endif
+
+    #ifdef debug
+    dat = "graf.txt";
+    #endif
 
     freopen(dat.c_str(), "r", stdin);
 
@@ -18,6 +54,57 @@ int main(void) {
     cin >> n;
     cin >> k;
 
+    for (int i = 0; i < n; i++) {
 
+        string redak;
+        cin >> redak;
+
+        susjedstva[i + 1] = {};
+        boje.push_back(0);
+
+        for (int j = 0; j < n; j++) {
+
+            if (redak[j] == '1') susjedstva[i + 1].push_back(j + 1);
+
+        }
+
+    }
+
+    #ifdef debug
+    for (int i = 0; i < n; i++) {
+
+        cout << i << ":";
+        for (int j: susjedstva[i]) cout << " " << j;
+
+        cout << '\n';
+
+    }
+    #endif
+
+
+    bool obojiv = false;
+
+    while (slj(n, k) && !obojiv) {
+
+        obojiv = true;
+
+        for (int i = 0; i < n && obojiv; i++) {
+
+            for (int j: susjedstva[i]) {
+
+                if (boje[i] != boje[j]) continue;
+
+                obojiv = false;
+                break;
+
+            }
+
+        }
+
+    }
+
+    cout << (int)obojiv << endl;
+
+    return 0;
 
 }
