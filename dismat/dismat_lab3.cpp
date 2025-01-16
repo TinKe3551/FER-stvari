4019cc4f7b9aa22893a01c41d453270fd6ae63d3
TinKe3551 <tk55945@fer.hr>
2025-01-16 12:14:37 +0100

    .

---
+++ dismat/dismat_lab3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+map<int, vector<int>> susjedstva;
+
+
+int main(void) {
+    
+    string  dat;
+    cin >> dat;
+
+    freopen(dat.c_str(), "r", stdin);
+
+    int n, k;
+    cin >> n;
+    cin >> k;
+
+
+
+}
