c0e099acd83ff970edce2183ee6375d905bc9ba1
TinKe3551 <tk55945@fer.hr>
2025-01-17 19:53:18 +0100

    .

--- dismat/dismat_lab3.cpp
+++ dismat/dismat_lab3.cpp
@@ -2,37 +2,144 @@
 #include <bits/stdc++.h>
 
 
+#define vpb 9223372036854775783
+#define verbint 1
+
+
 using namespace std;
 
 
 map<int, vector<int>> susjedstva;
 vector<int> boje;
+set<long long> stanja;
 
-//jebem ti ja git
 
-bool slj(int n, int k) {
+long long hash_kod(int &n, int &k) {
 
-    for (int i = 0; i < n; i++) {
+    long long h = 0;
 
-        boje[i]++;
-        boje[i] %= k;
+    long long m = 1;
 
-        if (boje[i] > 0) break;
+    map<int, int> preslikavanje;
+    int slj = 0;
 
-    }
-
-    // u početnom stanju popisa boja, u njemu se nalaze samo nule
-    // ako se popis opet nađe u takvom stanju, to znači
-    // da su sve moguće kombinacije boja isprobane
-
+    #ifdef verbose
+    cout << "preslikane boje: ";
+    #endif
     
     for (int i = 0; i < n; i++) {
-        // barem jedan broj različit od nule
-        if (boje[i] != 0) return true;
+
+        if (preslikavanje.count(boje[i]) == 0) preslikavanje[boje[i]] = slj++;
+        int j = preslikavanje[boje[i]];
+
+        #ifdef verbose
+        cout << j << " ";
+        #endif
+
+        m *= k;
+        m %= vpb;
+        h += (m * j) % vpb;
+
     }
 
-    // samo nule
-    return false;
+    #ifdef verbose
+    cout << '\n';
+    #endif
+
+    return h;
+
+}
+
+
+bool provjeri(bool &obojiv, int &n) {
+
+    if (obojiv) return true;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j: susjedstva[i]) {
+
+            if (boje[i] == boje[j]) {
+                return false;
+            }
+
+        }
+
+    }
+
+    obojiv = true;
+
+    #ifdef debug
+    cout << "----------------\n";
+    cout << "pronađen način bojanja: ";
+    for (int i: boje) cout << i << " ";
+    cout << '\n';
+    #endif
+
+    return true;
+
+}
+
+
+void boji(bool &obojiv, int &n, int &k, int dubina, int &kol_poziva) {
+
+    kol_poziva++;
+
+    if (provjeri(obojiv, n)) return;
+
+    #ifdef verbose
+    if (stanja.size() % verbint == 0) {
+        cout << "----------------\n";
+        cout << dubina << ". razina rekurzije\n";
+    }
+    #endif
+
+    long long stanje = hash_kod(n, k);
+
+    #ifdef verbose
+    if (stanja.size() % verbint == 0) {
+        cout << "kod stanja: " << stanje << '\n';
+        cout << "boje: "; for (int i: boje) cout << i << " "; cout << '\n';
+        cout << "stanje već prije zabilježeno: " << stanja.count(stanje) << '\n';
+        cout << "dosada zabilježeno stanja: " << stanja.size() << '\n';
+    }
+    #endif
+
+    if (stanja.count(stanje) > 0) return;
+    stanja.insert(stanje);
+
+    bool obojan = true;
+
+    set<int> promijeniti;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j: susjedstva[i]) {
+
+            if (boje[i] == boje[j]) {
+                
+                obojan = false;
+
+                promijeniti.insert(i);
+                promijeniti.insert(j);
+
+            }
+
+        }
+
+    }
+
+    for (int i: promijeniti) {
+
+        boje[i]++;
+        if (boje[i] < k) boji(obojiv, n, k, dubina + 1, kol_poziva);
+        boje[i]--;
+
+        boje[i]--;
+        if (boje[i] > 0) boji(obojiv, n, k, dubina + 1, kol_poziva);
+        boje[i]++;
+
+    }
 
 }
 
@@ -42,6 +149,7 @@ int main(void) {
     string  dat;
 
     #ifndef debug
+    cout << "naziv datoteke sa grafom > ";
     cin >> dat;
     #endif
 
@@ -55,6 +163,9 @@ int main(void) {
     cin >> n;
     cin >> k;
 
+    int max_deg = 0;
+    bool potpun = true;
+
     for (int i = 0; i < n; i++) {
 
         string redak;
@@ -63,15 +174,29 @@ int main(void) {
         susjedstva[i + 1] = {};
         boje.push_back(0);
 
+        int deg = 0;
+
         for (int j = 0; j < n; j++) {
 
-            if (redak[j] == '1') susjedstva[i + 1].push_back(j + 1);
+            if (redak[j] == '1') {
+                susjedstva[i].push_back(j);
+                deg++;
+            }
 
         }
 
+        if (deg > max_deg) max_deg = deg;
+        if (deg < n - 1) potpun = false;
+
     }
 
-    #ifdef debug
+    #ifdef verbose
+    
+    cout << "najveći stupanj: " << max_deg << '\n';
+
+    if (potpun) cout << "graf je potpun\n";
+    else cout << "graf nije potpun\n";
+
     for (int i = 0; i < n; i++) {
 
         cout << i << ":";
@@ -83,32 +208,24 @@ int main(void) {
     #endif
 
 
-    bool obojiv = false;
-
-    while (slj(n, k) && !obojiv) {
-
-        for (int i: boje) cout << i << " ";
-        cout << '\n';
-
-        obojiv = true;
-
-        for (int i = 0; i < n && obojiv; i++) {
-
-            for (int j: susjedstva[i]) {
-
-                if (boje[i] != boje[j]) continue;
-
-                obojiv = false;
-                break;
-
-            }
-
-        }
-
+    bool obojiv = k >= max_deg;
+    if (potpun && k < n) {
+        cout << 0;
+        return 0;
     }
 
-    cout << (int)obojiv << endl;
+    int kol_poziva = 0;
+
+    boji(obojiv, n, k, 0, kol_poziva);
+
+    cout << obojiv << '\n';
+
+    #ifdef debug
+    cout << "količina poziva funkcije: " << kol_poziva << '\n';
+    #endif
 
     return 0;
 
 }
+
+
