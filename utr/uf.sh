db5ef2a7920ca44141510196233a4cb142c38708
TinKe3551 <tin.keglevic@gmail.com>
2025-03-26 22:09:58 +0100

    brdo testova za labos 2 iz utr

--- utr/uf.sh
+++ utr/uf.sh
@@ -1,6 +1,5 @@
 for i in $(seq 15 2214);
 do
     echo $i
-    time ./program < "lab2_primjeri/test$i""/t.ul" > "lab2_primjeri/test$i""/t.iz"
-    # rm -rf lab2_primjeri/test$i
+    rm -rf lab2_primjeri/test$i
 done
