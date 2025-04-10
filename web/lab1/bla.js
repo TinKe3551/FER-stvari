2226b5d5629a0b165ac15ba43787314050e75aba
TinKe3551 <tin.keglevic@gmail.com>
2025-04-10 22:11:08 +0200

    .

---
+++ web/lab1/bla.js
@@ -0,0 +1,4 @@
+let hehe = document.getElementById("ha")
+hehe.addEventListener("click", () => {
+    alert("Čestitamo osvojili ste 92834238943279 novih mobitela molim vas pošajite svoje bankovne podatke na moju adresu");
+});
