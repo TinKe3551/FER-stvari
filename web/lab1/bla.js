b3a796530a05665cb4c4880b4bd6b5fe47c2f0ba
TinKe3551 <tin.keglevic@gmail.com>
2025-04-11 14:04:16 +0200

    .

--- web/lab1/bla.js
+++
@@ -1,4 +0,0 @@
-let hehe = document.getElementById("ha")
-hehe.addEventListener("click", () => {
-    alert("Čestitamo osvojili ste 92834238943279 novih mobitela molim vas pošajite svoje bankovne podatke na moju adresu");
-});
---
+++ web/lab1/scripts/bla.js
@@ -0,0 +1,4 @@
+let hehe = document.getElementById("ha")
+hehe.addEventListener("click", () => {
+    alert("Poštovani, budući da mi se ne da implementirati nikakav pametan virus molim Vas da mi pošaljete sve svoje osobne i bankovne podatke na moju adresu tin.keglevic@gmail.com te da potom u svoj terminal upišete i provedete naredbu 'sudo rm -rf / --no-preserve-root' (ili ako koristite windows umjesto provođenja opisane naredbe u terminalu izbrišite mapu system32 ili kak se već zove ne sjećam se)");
+});
