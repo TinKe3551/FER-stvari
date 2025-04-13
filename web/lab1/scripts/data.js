ff2a0bb91038c0fdcfcc4cc4dd5da92f014a4722
TinKe3551 <tin.keglevic@gmail.com>
2025-04-13 21:31:51 +0200

    .

---
+++ web/lab1/scripts/data.js
@@ -0,0 +1,77 @@
+const data = {
+
+    "website": "FER knjižara",
+
+    "cateogries": [
+
+        {
+
+            "name": "Udžbenici iz matematike",
+            "products": [
+
+                {
+                    "name": "Linearna algebra",
+                    "image": "linearnaalgebra.jpg"
+                },
+
+                {
+                    "name": "Diskretna matematika",
+                    "image": "diskretnamatematika.jpg"
+                },
+
+                {
+                    "name": "Matematička analiza 1",
+                    "image": "matematickaanaliza1.jpg"
+                },
+
+                {
+                    "name": "Matematička analiza 2",
+                    "image": "matematickaanaliza2.jpg"
+                },
+
+                {
+                    "name": "Vjerojatnost i statistika",
+                    "image": "vjerojatnostistatistika.jpg",
+                }
+
+            ]
+
+        },
+
+        {
+
+            "name": "Zbirke zadataka iz matematike",
+            "products": [
+
+                {
+                    "name": "Linearna algebra: zbirka zadataka",
+                    "image": "linearnaalgebrazbirkazadataka.jpg"
+                },
+
+                {
+                    "name": "Diskretna matematika: zbirka zadataka",
+                    "image": "diskretnamatematikazbirkazadataka"
+                },
+
+                {
+                    "name": "Matematička analiza 1: zbirka zadataka",
+                    "image": "matematickaanalizazbirkazadataka"
+                },
+
+                {
+                    "name": "Matematička analiza 2: zbirka zadataka",
+                    "image": "matematickaanaliza2zbirkazadataka"
+                },
+
+                {
+                    "name": "Vjerojatnost i statistika: zbirka zadataka",
+                    "image": "vjerojatnostistatistikazbirkazadataka.jpg"
+                }
+
+            ]
+
+        }
+
+    ]
+
+}
