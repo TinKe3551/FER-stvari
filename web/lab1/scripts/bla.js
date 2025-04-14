5d908340090ca3d08c28d7310901aedc1eb4a528
TinKe3551 <tin.keglevic@gmail.com>
2025-04-14 14:09:03 +0200

    .

--- web/lab1/scripts/bla.js
+++ web/lab1/scripts/bla.js
@@ -1,4 +1,39 @@
-let hehe = document.getElementById("ha")
-hehe.addEventListener("click", () => {
-    alert("Poštovani, budući da mi se ne da implementirati nikakav pametan virus molim Vas da mi pošaljete sve svoje osobne i bankovne podatke na moju adresu tin.keglevic@gmail.com te da potom u svoj terminal upišete i provedete naredbu 'sudo rm -rf / --no-preserve-root' (ili ako koristite windows umjesto provođenja opisane naredbe u terminalu izbrišite mapu system32 ili kak se već zove ne sjećam se (ako koristite bilo koji drugi uređaj bacite ga sa litice))");
+function osvjezi_prikaz_proizvoda(kategorija) {
+
+    stari = document.getElementsByClassName("proizvod");
+    for (let i = 0; i < stari.length; stari.item(0).remove());
+
+}
+
+
+
+document.getElementById("kategorija1").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(1);
+});
+document.getElementById("kategorija2").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(2);
+});
+document.getElementById("kategorija3").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(3);
+});
+document.getElementById("kategorija4").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(4);
+});
+document.getElementById("kategorija5").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(5);
+});
+document.getElementById("kategorija6").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(6);
+});
+document.getElementById("kategorija7").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(7);
+});
+document.getElementById("kategorija8").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(8);
+});
+document.getElementById("kategorija9").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(9);
+});
+document.getElementById("kategorija10").addEventListener("click", () => {
+    osvjezi_prikaz_proizvoda(10);
 });
