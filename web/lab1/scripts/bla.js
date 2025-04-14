aef8234d6f54f33952fdeb1f2450219cfa6973c7
TinKe3551 <tin.keglevic@gmail.com>
2025-04-14 16:56:24 +0200

    .

--- web/lab1/scripts/bla.js
+++ web/lab1/scripts/bla.js
@@ -3,7 +3,23 @@ function osvjezi_prikaz_proizvoda(kategorija) {
     stari = document.getElementsByClassName("proizvod");
     for (let i = 0; i < stari.length; stari.item(0).remove());
 
+    
+    let prikaz = document.getElementById("proizvodi");
+    novi = data["categories"][kategorija];
 
+    for (let i = 0; i < novi.length ; i++) {
+
+        let proizvod = document.createElement("div");
+
+        //proizvod.appendChild(document.createElement("div"));
+        proizvod.className = "proizvod";
+        proizvod.innerHTML = novi[i]["name"];
+        //proizvod.childNodes.item(0).class = "proizvodslika";
+        //proizvod.childNodes.item(1).class = "proizvodtekst";
+
+        prikaz.appendChild(proizvod);
+
+    }
 
 }
 
@@ -28,10 +44,10 @@ document.getElementById("kategorija6").addEventListener("click", () => {
     osvjezi_prikaz_proizvoda("Zbirke zadataka iz računarskih predmeta");
 });
 document.getElementById("kategorija7").addEventListener("click", () => {
-    osvjezi_prikaz_proizvoda("Udžbenici iz ostalih predmeta");
+    osvjezi_prikaz_proizvoda("Udžbenici i zbirke zadataka iz ostalih predmeta");
 });
 document.getElementById("kategorija8").addEventListener("click", () => {
-    osvjezi_prikaz_proizvoda("Zbirke zadataka iz ostalih predmeta");
+    osvjezi_prikaz_proizvoda("Bilježnice sa stranicama s crtama");
 });
 document.getElementById("kategorija9").addEventListener("click", () => {
     osvjezi_prikaz_proizvoda("Bilježnice s kariranim stranicama");
--- web/lab1/scripts/data.js
+++ web/lab1/scripts/data.js
@@ -2,76 +2,298 @@ const data = {
 
     "website": "FER knjižara",
 
-    "cateogries": [
+    "categories": {
 
-        {
+        "Udžbenici iz matematike": [
 
-            "name": "Udžbenici iz matematike",
-            "products": [
+            {
+                "name": "Matematička analiza 1",
+                "image": "matan1udz.png"
+            },
 
-                {
-                    "name": "Linearna algebra",
-                    "image": "linearnaalgebra.jpg"
-                },
+            {
+                "name": "Matematička analiza 2",
+                "image": "matan2udz.png"
+            },
 
-                {
-                    "name": "Diskretna matematika",
-                    "image": "diskretnamatematika.jpg"
-                },
+            {
+                "name": "Matematička analiza 3",
+                "image": "matan3udz.png"
+            },
 
-                {
-                    "name": "Matematička analiza 1",
-                    "image": "matematickaanaliza1.jpg"
-                },
+            {
+                "name": "Linearna algebra",
+                "image": "linearnaudz.png"
+            },
 
-                {
-                    "name": "Matematička analiza 2",
-                    "image": "matematickaanaliza2.jpg"
-                },
+            {
+                "name": "Vjerojatnost i statistika",
+                "image": "visudz.png"
+            }
 
-                {
-                    "name": "Vjerojatnost i statistika",
-                    "image": "vjerojatnostistatistika.jpg",
-                }
+        ],
 
-            ]
+        "Zbirke zadataka iz matematike": [
 
-        },
+            {
+                "name": "Matematička analiza 1: zbirka zadataka",
+                "image": "matan1zbirka.png"
+            },
 
-        {
+            {
+                "name": "Matematička analiza 2: zbirka zadataka",
+                "image": "matan2zbirka.png"
+            },
 
-            "name": "Zbirke zadataka iz matematike",
-            "products": [
+            {
+                "name": "Matematička analiza 3: zbirka zadataka",
+                "image": "matan3zbirka.png"
+            },
 
-                {
-                    "name": "Linearna algebra: zbirka zadataka",
-                    "image": "linearnaalgebrazbirkazadataka.jpg"
-                },
+            {
+                "name": "Linearna algebra: zbirka zadataka",
+                "image": "linearnazbirka.png"
+            },
 
-                {
-                    "name": "Diskretna matematika: zbirka zadataka",
-                    "image": "diskretnamatematikazbirkazadataka"
-                },
+            {
+                "name": "Vjerojatnost i statistika: zbirka zadataka",
+                "image": "viszbirka.png"
+            }
 
-                {
-                    "name": "Matematička analiza 1: zbirka zadataka",
-                    "image": "matematickaanalizazbirkazadataka"
-                },
+        ],
 
-                {
-                    "name": "Matematička analiza 2: zbirka zadataka",
-                    "image": "matematickaanaliza2zbirkazadataka"
-                },
+        "Udžbenici iz elektrotehničkih predmeta": [
 
-                {
-                    "name": "Vjerojatnost i statistika: zbirka zadataka",
-                    "image": "vjerojatnostistatistikazbirkazadataka.jpg"
-                }
+            {
+                "name": "Osnove elektrotehnike",
+                "image": "osneleudz.png"
+            },
 
-            ]
+            {
+                "name": "Električni krugovi",
+                "image": "elektruudz.png"
+            },
 
-        }
+            {
+                "name": "Elektronika 1",
+                "image": "el1udz"
+            },
 
-    ]
+            {
+                "name": "Elektronika 2",
+                "image": "el2udz"
+            },
+
+            {
+                "name": "Signali i sustavi",
+                "image": "sisudz.png"
+            }
+
+        ],
+
+        "Zbirke zadataka iz elektrotehničkih predmeta": [
+
+            {
+                "name": "Osnove elektrotehnike: zbirka zadataka",
+                "image": "osnelezbirka.png"
+            },
+
+            {
+                "name": "Električni krugovi: zbirka zadataka",
+                "image": "elektruzbirka.png"
+            },
+
+            {
+                "name": "Elektronika 1: zbirka zadataka",
+                "image": "el1zbirka.png"
+            },
+
+            {
+                "name": "Elektronika 2: zbirka zadataka",
+                "image": "el2zbirka.png"
+            },
+
+            {
+                "name": "Signali i sustavi: zbirka zadataka",
+                "image": "siszbirka.png"
+            }
+
+        ],
+
+        "Udžbenici iz računarskih predmeta": [
+
+            {
+                "name": "Uvod u programiranje",
+                "image": "uproudz.png"
+            },
+
+            {
+                "name": "Objektno orijentirano programiranje",
+                "image": "oopudz.png"
+            },
+
+            {
+                "name": "Natjecateljsko programiranje",
+                "image": "natproudz.png"
+            },
+
+            {
+                "name": "Skriptni jezici",
+                "image": "skrjezudz.png"
+            },
+
+            {
+                "name": "Uvod u teoriju računarstva",
+                "image": "utrudz.png"
+            }
+
+        ],
+
+        "Zbirke zadataka iz računarskih predmeta": [
+
+            {
+                "name": "Uvod u programiranje: zbirka zadataka",
+                "image": "uprozbirka.png"
+            },
+
+            {
+                "name": "Objektno orijentirano programiranje: zbirka zadataka",
+                "image": "oopzbirka.png"
+            },
+
+            {
+                "name": "Natjecateljsko programiranje: zbirka zadataka",
+                "image": "natprozbirka.png"
+            },
+
+            {
+                "name": "Skriptni jezici: zbirka zadataka",
+                "image": "skrjezzbirka.png"
+            },
+
+            {
+                "name": "Uvod u teoriju računarstva: zbirka zadataka",
+                "image": "utrzbirka.png"
+            }
+
+        ],
+
+        "Udžbenici i zbirke zadataka iz ostalih predmeta": [
+
+            {
+                "name": "Inženjerska ekonomika 1",
+                "image": "inzeko1udz.png"
+            },
+
+            {
+                "name": "Inženjerska ekonomika 1: zbirka zadataka",
+                "image": "inzeko1zbirka.png"
+            },
+
+            {
+                "name": "Inženjerska ekonomika 2",
+                "image": "inzeko2udz.png"
+            },
+
+            {
+                "name": "Inženjerska ekonomika 2: zbirka zadataka",
+                "image": "inzeko2zbirka"
+            },
+
+            {
+                "name": "Korištenje operacijskog sustava Linux",
+                "image": "kosludz.png"
+            }
+
+        ],
+
+        "Bilježnice sa stranicama s crtama": [
+
+            {
+                "name": "Bilježnica s crtama 25 listova",
+                "image": "biljcrte.png"
+            },
+
+            {
+                "name": "Bilježnica s crtama 30 listova",
+                "image": "biljcrte.png"
+            },
+
+            {
+                "name": "Bilježnica s crtama 35 listova",
+                "image": "biljcrte.png"
+            },
+
+            {
+                "name": "Bilježnica s crtama 40 listova",
+                "image": "biljcrte.png"
+            },
+
+            {
+                "name": "Bilježnica s crtama 45 listova",
+                "image": "biljcrte.png"
+            }
+
+        ],
+
+        "Bilježnice s kariranim stranicama": [
+
+            {
+                "name": "Bilježnica karirana 25 listova",
+                "image": "biljkaro.png"
+            },
+
+            {
+                "name": "Bilježnica karirana 30 listova",
+                "image": "biljkaro.png"
+            },
+
+            {
+                "name": "Bilježnica karirana 35 listova",
+                "image": "biljkaro.png"
+            },
+
+            {
+                "name": "Bilježnica karirana 40 listova",
+                "image": "biljkaro.png"
+            },
+
+            {
+                "name": "Bilježnica karirana 45 listova",
+                "image": "biljkaro.png"
+            }            
+
+        ],
+
+        "Bilježnice s praznim stranicama": [
+
+            {
+                "name": "Bilježnica prazna 25 listova",
+                "image": "biljprazna.png"
+            },
+
+            {
+                "name": "Bilježnica prazna 30 listova",
+                "image": "biljprazna.png"
+            },
+
+            {
+                "name": "Bilježnica prazna 35 listova",
+                "image": "biljprazna.png"
+            },
+
+            {
+                "name": "Bilježnica prazna 40 listova",
+                "image": "biljprazna.png"
+            },
+
+            {
+                "name": "Bilježnica prazna 45 listova",
+                "image": "biljprazna.png"
+            }
+
+        ]
+
+    }
 
 }
