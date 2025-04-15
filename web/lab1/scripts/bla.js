e9e1281beacc033a6df231ef21cbd8dda01e5734
TinKe3551 <tin.keglevic@gmail.com>
2025-04-16 01:55:58 +0200

    .

--- web/lab1/scripts/bla.js
+++ web/lab1/scripts/bla.js
@@ -21,13 +21,33 @@ function osvjezi_prikaz_proizvoda(kategorija) {
         slika.alt = "slika za " + novi[i]["name"];
         slika.className = "proizvod";
 
+        let ikonica = document.createElement("img");
+        ikonica.src = "./images/edgarferhr.png";
+        ikonica.className = "nevidljivo";
+        ikonica.style.opacity = 0;
+        ikonica.style.width = 0;
+        ikonica.id = "pr_" + i.toString() + "_ik";
+
         let tekst = document.createElement("p");
         tekst.innerHTML = novi[i]["name"];
-
         
         proizvod.appendChild(slika);
-        proizvod.appendChild(document.createElement("br"));
+        proizvod.appendChild(ikonica);
         proizvod.appendChild(tekst);
+
+        proizvod.addEventListener("mouseenter", () => {
+            let ik = document.getElementById("pr_" + i.toString() + "_ik");
+            ik.style.opacity = 1;
+            ik.style.width = "40px";
+        })
+
+        proizvod.addEventListener("mouseleave", () => {
+            let ik = document.getElementById("pr_" + i.toString() + "_ik");
+            ik.style.opacity = 0;
+            ik.style.width = 0;
+        })
+
+        proizvod.style.cursor = "pointer";
 
         prikaz.appendChild(proizvod);
 
