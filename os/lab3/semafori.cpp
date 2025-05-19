ed39ea83ac8e78028cfd3ac44cf6e2ae5782d10c
TInKe3551 <tin.keglevic@gmail.com>
2025-05-19 17:35:26 +0200

    .

--- os/lab3/semafori.cpp
+++ os/lab3/semafori.cpp
@@ -9,8 +9,11 @@ using std::cout;
 using std::string;
 
 
-sem_t *sem_kupci_ulazak = (sem_t*) malloc(sizeof(sem_t));
-sem_t *sem_sastojci = (sem_t*) malloc(sizeof(sem_t));
+sem_t *sem_ulazak_kupci0;
+sem_t *sem_ulazak_kupci1;
+sem_t *sem_ulazak_kupci2;
+
+sem_t *sem_sastojci;
 
 
 string sastojci[3] = {
@@ -21,11 +24,6 @@ string sastojci[3] = {
 
 int *stol;
 
-// za vrste kupaca 0, 1 i 2 informacija o tome je li u trgovini prisutan kupac te vrste
-int *kv0;
-int *kv1;
-int *kv2;
-
 
 void proc_trgovac()
 {
@@ -34,27 +32,37 @@ void proc_trgovac()
 
     usleep(500000);
 
+    int a = 2;
 
     while (1) {
 
         trgovac_sastojci.clear();
 
-        sleep(1);
+        // sleep(1);
 
         // cekaj_semafor_za_sastojke_na_stolu();
         sem_wait(sem_sastojci);
 
         // postavi_sastojke_na_stol();
-        srand(time(nullptr));
-        int vrsta = rand() % 3;
+
+        if (*stol == -1) {
+
+            // TODO iz nekog razloga trgovac uvijek stavlja istu vrstu proizvoda pa to treba popraviti
+            // zasada koristim ovu zakrpu
+            int vrsta = (time(nullptr) + a++ % 3) % 3;
 
 
-        for (int i = 0; i < 3; i++) {
-            if (i != vrsta)
-                trgovac_sastojci.push_back(sastojci[i]);
+            for (int i = 0; i < 3; i++) {
+                if (i != vrsta)
+                    trgovac_sastojci.push_back(sastojci[i]);
+            }
+
+            cout << "trgovac: " << trgovac_sastojci[0] << " i " << trgovac_sastojci[1] << '\n';
+
+            *stol = vrsta;
+            cout << "stol: " << *stol << "\n\n";
+
         }
-
-        cout << "trgovac: " << trgovac_sastojci[0] << " i " << trgovac_sastojci[1] << '\n';
 
         // postavi_semafor_za_sastojke_na_stolu();
         sem_post(sem_sastojci);
@@ -73,23 +81,74 @@ void proc_kupac(int vrsta_kupca, int broj_kupca)
     cout << "kupac " << broj_kupca << ": " << kupac_sastojak << '\n';
 
     // cekaj_semafor_za_uci_u_trgovinu();
-    sem_wait(sem_kupci_ulazak);
+    switch (vrsta_kupca) {
 
-    // if (u trgovini nema kupac iste vrste) uđi();
+        case 0:
+            sem_wait(sem_ulazak_kupci0);
+            break;
+
+        case 1:
+            sem_wait(sem_ulazak_kupci1);
+            break;
+
+        case 2:
+            sem_wait(sem_ulazak_kupci2);
+            break;
+        
+        default:
+            cout << "????\n";
+            break;
+
+    }
 
     // while (nema potrebnih sastojaka) {
     //     cekaj_semafor_za_sastojke_na_stolu();
     //     pogledaj_sastojke();
     //     if (jos nema sastojaka) postavi_semafor_za_sastojke_na_stolu();
     // }
+    int sastojci_prisutni = 0;
+
+    while (!sastojci_prisutni) {
+
+        sem_wait(sem_sastojci);
+
+        // cout << "kupac " << broj_kupca << " (" << vrsta_kupca << ")" << " vidi: " << *stol << '\n';
+
+        if(*stol == vrsta_kupca)
+            sastojci_prisutni++;
+
+        else 
+            sem_post(sem_sastojci);
+
+    }
 
     // uzmi_sastojke();
-    // postavi_semafor_za_sastojke_na_stolu();
-
     cout << "kupac " << broj_kupca << " sastavlja sendvič i jede\n\n";
+    *stol = -1;
+    
+    // postavi_semafor_za_sastojke_na_stolu();
+    sem_post(sem_sastojci);
 
     // postavi_semafor_za_uci_u_trgovinu();
-    sem_post(sem_kupci_ulazak);
+    switch (vrsta_kupca) {
+
+        case 0:
+            sem_post(sem_ulazak_kupci0);
+            break;
+
+        case 1:
+            sem_post(sem_ulazak_kupci1);
+            break;
+
+        case 2:
+            sem_post(sem_ulazak_kupci2);
+            break;
+        
+        default:
+            cout << "????\n";
+            break;
+
+    }
 
     return;
 
@@ -99,13 +158,29 @@ void proc_kupac(int vrsta_kupca, int broj_kupca)
 int main(void)
 {
 
-    // semafor za kupce koji još nisu ušli u trgovinu
+    // semafor za kupce vrste 0 koji još nisu ušli u trgovinu
     int ID = shmget(IPC_PRIVATE, sizeof(sem_t), 0600);
-    sem_kupci_ulazak = (sem_t*) shmat(ID, NULL, 0);
+    sem_ulazak_kupci0 = (sem_t*) shmat(ID, NULL, 0);
     shmctl (ID, IPC_RMID, NULL);
 
-    sem_init(sem_kupci_ulazak, 1, 1);
-    sem_post(sem_kupci_ulazak);
+    sem_init(sem_ulazak_kupci0, 1, 1);
+    sem_post(sem_ulazak_kupci0);
+
+    // semafor za kupce vrste 1 koji još nisu ušli u trgovinu
+    ID = shmget(IPC_PRIVATE, sizeof(sem_t), 0600);
+    sem_ulazak_kupci1 = (sem_t*) shmat(ID, NULL, 0);
+    shmctl (ID, IPC_RMID, NULL);
+
+    sem_init(sem_ulazak_kupci1, 1, 1);
+    sem_post(sem_ulazak_kupci1);
+
+    // semafor za kupce vrste 2 koji još nisu ušli u trgovinu
+    ID = shmget(IPC_PRIVATE, sizeof(sem_t), 0600);
+    sem_ulazak_kupci2 = (sem_t*) shmat(ID, NULL, 0);
+    shmctl (ID, IPC_RMID, NULL);
+
+    sem_init(sem_ulazak_kupci2, 1, 1);
+    sem_post(sem_ulazak_kupci2);
 
     // semafor za trgovca i kupce koji su u trgovini
     ID = shmget(IPC_PRIVATE, sizeof(sem_t), 0600);
@@ -115,23 +190,6 @@ int main(void)
     sem_init(sem_sastojci, 1, 1);
     sem_post(sem_sastojci);
 
-    // varijable koje naznačuju je li u trgovini
-    // prisutan kupac određene vrste
-    ID = shmget(IPC_PRIVATE, sizeof(int), 0600);
-    kv0 = (int*) shmat(ID, NULL, 0);
-    shmctl(ID, IPC_RMID, NULL);
-    *kv0 = 0;
-
-    ID = shmget(IPC_PRIVATE, sizeof(int), 0600);
-    kv1 = (int*) shmat(ID, NULL, 0);
-    shmctl(ID, IPC_RMID, NULL);
-    *kv1 = 0;
-
-    ID = shmget(IPC_PRIVATE, sizeof(int), 0600);
-    kv2 = (int*) shmat(ID, NULL, 0);
-    shmctl(ID, IPC_RMID, NULL);
-    *kv2 = 0;
-
     // preko ove varijable određuje se koji su sastojci na stolu
     // -1 - stol je prazan
     // 0 - sir i šunka
@@ -140,7 +198,7 @@ int main(void)
     ID = shmget(IPC_PRIVATE, sizeof(int), 0600);
     stol = (int*) shmat(ID, NULL, 0);
     shmctl(ID, IPC_RMID, NULL);
-    *stol = 0;
+    *stol = -1;
 
     int f = fork();
 
@@ -196,8 +254,8 @@ int main(void)
         cout << "??\n";
     }
 
-    sem_destroy(sem_kupci_ulazak);
-    shmdt(sem_kupci_ulazak);
+    sem_destroy(sem_ulazak_kupci0);
+    shmdt(sem_ulazak_kupci0);
 
     sem_destroy(sem_sastojci);
     shmdt(sem_sastojci);
