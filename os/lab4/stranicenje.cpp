05ba3426c615a04e0b3b2c82f56cc454e3897e3b
TInKe3551 <tin.keglevic@gmail.com>
2025-05-22 13:14:21 +0200

    .

--- os/lab4/stranicenje.cpp
+++ os/lab4/stranicenje.cpp
@@ -1,21 +1,16 @@
 #include <bits/stdc++.h>
 
 
-void izvjestaj() {
-
-}
-
-
 int main(void) {
 
     using namespace std;
 
-    int n_proc;
-    int n_str;
-    int n_okv;
-    int vel_str;
-    int vel_prog;
-    int vel_mem;
+    int n_proc; // koliko procesa
+    int n_str; // koliko stranica po procesu
+    int n_okv; // koliko okvira
+    int vel_str; // koliko riječi po stranici
+    int vel_prog; // koliko riječi po programu
+    int vel_mem; // koliko riječi u primarnoj memoriji
 
     cout << "količina procesa: ";
     cin >> n_proc;
@@ -32,51 +27,77 @@ int main(void) {
     vel_prog = vel_str * n_str;
     vel_mem = vel_str * n_okv;
 
-    srand(time(nullptr) + clock());
-
     int okviri[n_okv][2]; // {broj procesa, broj stranice)
     int A_zast[n_proc][n_str + 1];
 
     int i = 0;
-    int n_koristenih_okvira = 0;
+    int n_kor_okv = 0;
     while (1) {
 
-        sleep(rand() % 3 + 1);
+        sleep(3);
 
         int proc = rand() % n_proc;
-        int trazena_adr = rand() % vel_prog + 1 + proc * vel_prog;
-        int trazena_str = trazena_adr / vel_str;
+        int trazena_adr = rand() % vel_prog + 1;
+        int trazena_str = (trazena_adr - 1) / vel_str + 1;
+        trazena_adr += proc * vel_prog;
 
-        cout << "proces " << proc + 1 << " traži pristup adresi " << trazena_adr;
+        cout << "\nproces " << proc + 1 << " traži pristup adresi " << trazena_adr
+        << " (" << trazena_str << ". stranica)\n";
 
-        if (n_koristenih_okvira < n_okv) {
+        bool pogodak = false;
+        for (int j = 0; j < n_kor_okv && !pogodak; j++) {
+            if (okviri[j][0] == proc && okviri[j][1] == trazena_str) {
+                pogodak = true;
+            }
+        }
+
+        if (pogodak) {
+            cout << "++pogodak++\n";
+            A_zast[proc][trazena_str] = 1;
+        }
+
+        else if (n_kor_okv < n_okv) {
+
+            cout << "--promašaj--\n";
 
             okviri[i][0] = proc;
             okviri[i][1] = trazena_str;
 
             i = (i + 1) % n_okv;
 
-            for (int j = 0; j < n_okv; j++) {
-                cout << j << ". okvir: proc " << okviri[j][0] + 1 << " str " << okviri[j][1] 
-                << " (" << A_zast[okviri[j][0]][okviri[j][1]] << '\n';
-            }
+            n_kor_okv++;
+
+            A_zast[proc][trazena_str] = 1;
 
         }
 
-        bool pogodak = false;
-        for (int j = 0; j < n_okv && !pogodak; j++) {
-            if (okviri[j][0] == proc && okviri[j][1] == trazena_str) {
-                cout << "pogodak\n";
-                pogodak = true;
-            }
-        }
-        if (pogodak) continue;
+        else {
 
-        while (A_zast[okviri[i][0]][okviri[i][1]] != 0) {
-            A_zast[okviri[i][0]][okviri[i][1]] = 0;
+            cout << "--promašaj--\n";
+
+            while (A_zast[okviri[i][0]][okviri[i][1]] != 0) {
+                A_zast[okviri[i][0]][okviri[i][1]] = 0;
+                i = (i + 1) % n_okv;
+            }
+
+            okviri[i][0] = proc;
+            okviri[i][1] = trazena_str;
+            A_zast[proc][trazena_str] = 1;
             i = (i + 1) % n_okv;
+        
+        }
+
+        for (int j = 0; j < n_kor_okv; j++) {
+            
+            if (i == j) cout << "> ";
+            
+            cout << j + 1 << ". okvir: proc " << okviri[j][0] + 1 << " str " << okviri[j][1] 
+            << " (A=" << A_zast[okviri[j][0]][okviri[j][1]] << ")\n";
+
         }
 
     }
+
+    return 0;
 
 }
