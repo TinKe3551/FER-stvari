b8367afbbc4fd17b950dc64f4751ad8962164fba
TInKe3551 <tin.keglevic@gmail.com>
2025-05-25 21:24:40 +0200

    .

---
+++ utr/utrlab4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <cstddef>
+
+
+using namespace std;
+
+
+map<string, vector<string>> produkcije = {
+    {"S", {"aAB", "bBA"}},
+    {"A", {"bC", "a"}},
+    {"B", {"ccSbc", ""}},
+    {"C", {"AA"}}
+};
+
+deque<char> nezavrsni;
+vector<string> rekreacija;
+
+
+vector<string> rekreiraj(string &zadani) {
+    
+    
+
+}
+
+
+int main(void) {
+
+    string zadani;
+    cin >> zadani;
+
+    rekreiraj(zadani);
+
+    return 0;
+}
