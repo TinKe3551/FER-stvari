9069b8a93192740b024f1cb95cca367e0e76db01
TInKe3551 <tin.keglevic@gmail.com>
2025-06-10 11:12:30 +0200

    .

---
+++ web/watchwhathappenswheniwriteaprogramidontknow/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.post('/', function(req, res) {
+res.send('<p>Poštovani,</p><p>wdoawhdwhndawdiaudocsngfucngsufgnasogfasgfsrgfsaof.</p><p>Lp,</p>')
+});
+app.listen(3000);
