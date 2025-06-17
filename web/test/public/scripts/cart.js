c75fc8de12909157c84553fffe8eb06ba5397e18
TInKe3551 <tin.keglevic@gmail.com>
2025-06-17 09:01:06 +0200

    .

--- web/test/public/scripts/cart.js
+++ web/test/public/scripts/cart.js
@@ -1,34 +1,6 @@
-for (let gumb_dodavanja in document.getElementsByClassName(
-    "kosr_proizvod_dodaj",
-)) {
-    gumb_dodavanja.addEventListener("click", () => {
-        let id = "br_" + gumb_dodavanja.id;
-        console.log(id);
-        let brojac = document.getElementById(id).innerHTML;
-        brojac = parseInt(brojac);
-        document.getElementById(id).innerHTML = brojac + 1;
+for (let i = 0; true; i++) {
+    let gumb_dodavanja = document.getElementById("gd_" + 1);
+    if (gumb_dodavanja === null) break;
 
-        brojac = document.getElementById("kosr_brojac").innerHTML;
-        brojac = parseInt(brojac);
-        document.getElementById(id).innerHTML = brojac + 1;
-    });
-}
-
-for (let gumb_dodavanja in document.getElementsByClassName(
-    "kosr_proizvod_makni",
-)) {
-    gumb_dodavanja.addEventListener("click", () => {
-        let id = "br_" + gumb_dodavanja.id;
-        let brojac = document.getElementById(id).innerHTML;
-        brojac = parseInt(brojac);
-        document.getElementById(id).innerHTML = brojac - 1;
-
-        brojac = document.getElementById("kosr_brojac").innerHTML;
-        brojac = parseInt(brojac);
-        document.getElementById(id).innerHTML = brojac - 1;
-    });
-}
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
+    gumb_dodavanja.addEventListener(() => {});
 }
