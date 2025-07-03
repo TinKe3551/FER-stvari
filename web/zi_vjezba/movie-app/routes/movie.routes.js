ac44e2870cfc1cca19d61584bd2c05c7bea83e93
TinKe3551 <tin.keglevic@gmail.com>
2025-07-03 09:29:01 +0200

    .

--- web/zi_vjezba/movie-app/routes/movie.routes.js
+++ web/zi_vjezba/movie-app/routes/movie.routes.js
@@ -1,17 +1,52 @@
 var express = require("express");
 var router = express.Router();
 
+// ova glupost je potrebna kako bih mogao dohvatiti varijable iz post requestova koje su zadane preko formi od kojih su došli ti post requestovi
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 
-router.get("/movies")
+var movies = [
+    "YEET", "Yeet", 
+    "aabaabababababaaaababab", "kontekstno neovisna gramatika G",
+    "Kako napraviti funkcionalan virtualni stroj", "nitko iz ztela ikad"
+];
 
 
-router.get("/movies/add")
+router.get("/movies", (req, res, next) => {
+
+    res.render("movieList", {
+        movies: movies,
+    })
+
+})
 
 
-router.post("/movies/add")
+router.get("/movies/add", (req, res, next) => {
+
+    res.render("addMovie");
+
+})
 
 
-router.post("/movies/delete/:id")
+router.post("/movies/add", urlencodedParser, (req, res, next) => {
+
+    movies.push(req.body.movieTitle);
+    movies.push(req.body.director);
+
+    res.redirect("/movies");
+
+})
+
+
+router.post("/movies/delete/:id", (req, res, next) => {
+
+    let i = parseInt(req.params.id.substring(1));
+
+    movies.splice(i, 2);
+
+    res.redirect("/movies");
+
+})
 
 
 module.exports = router;
